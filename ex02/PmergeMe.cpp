2c72ff8cb8a8a8308465c72123dd781f72ba59c6
Lepokile <yahya.elboukili1@gmail.com>
2025-09-21 06:28:59 -0400

    ex02 in progress

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: yel-bouk <yel-bouk@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 12:59:13 by yel-bouk          #+#    #+#             */
-/*   Updated: 2025/09/20 16:11:04 by yel-bouk         ###   ########.fr       */
+/*   Updated: 2025/09/20 15:38:55 by yel-bouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ PmergeMe::PmergeMe(const std::vector<int>& input) : _vec(input),
 
 void PmergeMe::sortAll()
 {
-	std::sort(_vec.begin(), _vec.end());
-	std::sort(_deq.begin(), _deq.end());
+	// std::sort(_vec.begin(), _vec.end());
+	// std::sort(_deq.begin(), _deq.end());
+	fordJohnsonSort(_vec);
+	fordJohnsonSort(_deq);
 }
 
 void PmergeMe::printResults() const
@@ -34,4 +36,56 @@ void PmergeMe::printResults() const
 		std::cout << *it << ' ';
 	std::cout << std::endl;
 	
+}
+
+template <typename Container>
+void PmergeMe::fordJohnsonSort(Container& container)
+{
+    typedef typename Container::value_type T;
+
+    if (container.size() <= 1)
+        return;
+
+    // 1) Pair up elements and sort each pair (ascending)
+    std::vector< std::pair<T, T> > pairs;
+    pairs.reserve(container.size() / 2 + 1); // safe: pairs is a vector
+
+    typename Container::size_type i = 0;
+    for (; i + 1 < container.size(); i += 2) {
+        T a = container[i];
+        T b = container[i + 1];
+        if (a > b) {
+            T tmp = a; a = b; b = tmp;
+        }
+        pairs.push_back(std::make_pair(a, b));
+    }
+    // If odd count, duplicate the last to keep pairs aligned
+    if (i < container.size()) {
+        T x = container.back();
+        pairs.push_back(std::make_pair(x, x));
+    }
+
+    // 2) Recursively sort the first elements of each pair
+    std::vector<T> firsts;
+    firsts.reserve(pairs.size());
+    for (size_t k = 0; k < pairs.size(); ++k)
+        firsts.push_back(pairs[k].first);
+
+    // This template instantiates fine for std::vector<T> as well
+    fordJohnsonSort(firsts);
+
+    // 3) Merge: work in a vector (has reserve + predictable iterators)
+    std::vector<T> sorted;
+    sorted.reserve(container.size());
+    sorted.insert(sorted.end(), firsts.begin(), firsts.end());
+
+	for (size_t k = 0; k < pairs.size(); ++k) {
+		typename std::vector<T>::iterator it =
+			std::upper_bound(sorted.begin(), sorted.end(), pairs[k].second);
+		sorted.insert(it, pairs[k].second);
+	}
+
+
+    // Write back to the original container (vector/deque both support assign)
+    container.assign(sorted.begin(), sorted.end());
 }
